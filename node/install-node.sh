4cec5fa708c706b6dfd6b322aaba81c4b67484b2
duyvu997 <ngocduy0997@gmail.com>
2023-06-07 00:56:27 +0700

    update: add config for volta

--- node/install-node.sh
+++ node/install-node.sh
@@ -5,10 +5,16 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install Volta
-curl https://get.volta.sh | bash
+curl https://get.volta.sh | bash -s -- --skip-setup
+
+# Set VOLTA_HOME variable
+echo 'export VOLTA_HOME="$HOME/.volta"' >> ~/.bashrc
+
+# Add VOLTA_HOME/bin to the PATH variable
+echo 'export PATH="$VOLTA_HOME/bin:$PATH"' >> ~/.bashrc
 
 # Add Volta to the current shell session
-source ~/.volta/bin/volta
+source ~/.bashrc
 
 # Verify installations
 node -v
